9ff3283d7fb968bb66cccbfd209448b4e1124272
yasuak1 <yasuakimizuno36@gmail.com>
2022-07-02 14:30:19 +0900

    modify train_fnn

--- yasuak1/train_fnn.py
+++ yasuak1/train_fnn.py
@@ -8,26 +8,34 @@ from common.multi_layer_net_extend import MultiLayerNetExtend
 from common.trainer import Trainer
 
 # LOAD DATA
-train_path = '~/bio/data/train.npy'
-test_path = '~/bio/data/test.npy'
-train_dict = np.load(train_path).item()
+cnt = 100
+input_size = 480 * 640
+train_path = '/home/mizuno/data/neural_data/train.npy'
+test_path = '/home/mizuno/data/neural_data/test.npy'
+train_dict = np.load(train_path, allow_pickle=True).item()
+test_dict = np.load(test_path, allow_pickle=True).item()
+print(test_dict)
 x_train = train_dict['img_data']
 y_train = train_dict['label']
 x_test = test_dict['img_data']
 y_test = test_dict['label']
-
 """
+x_train = x_train.reshape(cnt, input_size)
+x_test = x_test.reshape(cnt, input_size)
+
+#print(x_train.shape)
+#print(y_train.shape)
+
 # Dropuoutの有無、割り合いの設定 ========================
 use_dropout = True  # Dropoutなしのときの場合はFalseに
 dropout_ratio = 0.5
 # ====================================================
 
-input_size = 480 * 640
 output_size = 2892
 network = MultiLayerNetExtend(input_size=input_size, hidden_size_list=[100],
                               output_size=output_size, use_dropout=use_dropout, dropout_ration=dropout_ratio)
-trainer = Trainer(network, x_train, t_train, x_test, t_test,
-                  epochs=301, mini_batch_size=100,
+trainer = Trainer(network, x_train, y_train, x_test, y_test,
+                  epochs=50, mini_batch_size=10,
                   optimizer='sgd', optimizer_param={'lr': 0.01}, verbose=True)
 trainer.train()
 
