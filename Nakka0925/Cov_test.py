28dfdb670daadbb0bb37472d71d1ba182c49ac78
nakanishi <h19s4040@hirosaki-u.ac.jp>
2022-07-20 13:22:13 +0900

    add cross-val

--- Nakka0925/Cov_test.py
+++ Nakka0925/Cov_test.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from imghdr import tests
 import sys, os
 sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import numpy as np
@@ -9,6 +10,7 @@ from datasets import data_gain
 from simple_convnet import SimpleConvNet
 from common.trainer import Trainer
 
+
 file_list, label_list = data_gain()
 
 file_list = np.array(file_list)
@@ -18,7 +20,10 @@ file_list = file_list.reshape(7550, 1, 192, 192)
 #0.0 ~ 1.0 に正規化
 file_list = [file.astype(float)/255.0 for file in file_list]
 
-train_x, test_x, train_y, test_y = train_test_split(file_list, label_list, test_size=0.2)
+
+train_x, test_x, train_y, test_y = train_test_split(file_list, label_list, test_size=0.2,
+                                                    stratify = label_list)
+
 
 train_x = np.array(train_x)
 test_x = np.array(test_x)
@@ -27,7 +32,8 @@ train_y = np.array(train_y)
 test_y = np.array(test_y)
 
 #層化抽出法を用いたK-分割交差検証
-kf = StratifiedKFold(n_splits=5, shuffle=True)
+splits = 5
+kf = StratifiedKFold(n_splits=splits, shuffle=True)
 all_loss = []
 all_val_loss = []
 all_acc = []
@@ -40,17 +46,17 @@ use_dropout = True  # Dropoutなしのときの場合はFalseに
 dropout_ratio = 0.5
 # ====================================================
 
-network = SimpleConvNet(input_dim=(1,192,192), 
-                        conv_param = {'filter_num': 8, 'filter_size': 3, 'pad': 0, 'stride': 1},
-                        hidden_size=100, output_size=3, weight_init_std='Relu', use_dropout = use_dropout,
-                        dropout_ration = dropout_ratio)
-
 for train_index, val_index in kf.split(train_x, train_y):              
     
     train_data  = train_x[train_index]
     train_label = train_y[train_index]
     val_data    = train_x[val_index]
     val_label   = train_y[val_index]
+
+    network = SimpleConvNet(input_dim=(1,192,192), 
+                        conv_param = {'filter_num': 8, 'filter_size': 3, 'pad': 0, 'stride': 1},
+                        hidden_size=64, output_size=3, weight_init_std='Relu', use_dropout = use_dropout,
+                        dropout_ration = dropout_ratio)
     
     trainer = Trainer(network, train_data, train_label, val_data, val_label, test_x, test_y,
                   epochs=max_epochs, batch_size = 128,
@@ -74,7 +80,8 @@ ave_all_acc = np.mean(all_acc, axis = 0)
 ave_all_val_acc = np.mean(all_val_acc, axis = 0)
 ave_all_loss = np.mean(all_loss, axis = 0)
 ave_all_val_loss = np.mean(all_val_loss, axis = 0)
-
+print(ave_all_acc)
+print(ave_all_loss)
 
 # パラメータの保存
 network.save_params("params.pkl")
